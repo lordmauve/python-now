ee38322c054eab77650ba040864c0ae983f6ed1a
vanessa <32312712+vlbee@users.noreply.github.com>
2020-02-06 21:27:13 +0000

    Add eval/exec toggle and exception handling

--- realPython.py
+++ realPython.py
@@ -1,8 +1,6 @@
-from browser import document, html, alert
+from browser import document, html, alert, markdown
 from io import StringIO
 import contextlib
-
-# bind event 'click' on button to function echo
 
 
 class editor():
@@ -12,6 +10,12 @@ class editor():
             self.run()
 
     def __init__(self, placeholderText="# Write some Python code here"):
+        self.exec = html.INPUT(type='radio', name='mode',
+                               value='exec', checked="checked")
+        self.eval = html.INPUT(type='radio', name='mode', value='eval')
+        # self.single = html.INPUT(type='radio', name='mode', value='single')
+        document <= self.exec + html.LABEL('Script')
+        document <= self.eval + html.LABEL('Expression')
 
         self.text = html.TEXTAREA(Class="textarea")
         self.text.value = placeholderText
@@ -22,14 +26,24 @@ class editor():
 
     def run(self):
         source = self.text.value
-        code = compile(source, 'input', 'exec')
+        mode = 'exec' if self.exec.checked else 'eval'
+        print(mode)
+        code = compile(source, 'input', mode)
         buff = StringIO()
-        with contextlib.redirect_stdout(buff):
-            exec(code)
+        with contextlib.redirect_stdout(buff), contextlib.redirect_stderr(buff):
+            try:
+                result = exec(code, {})
+            except:
+                import traceback
+                traceback.print_exc()
+            else:
+                if result is not None:
+                    print(repr(result))
+                else:
+                    print('None')
         self.output.text = buff.getvalue()
 
 
-editor()
 editor("""
 from itertools import islice
 def fib():
